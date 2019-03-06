c89fb3fe7cdeb12667f532321ba35c6cbd784f4b
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-06 10:47:00 -0600

    Created Address model

--- Sources/App/Configuration/migrations.swift
+++ Sources/App/Configuration/migrations.swift
@@ -2,4 +2,5 @@ import Fluent
 
 func migrations(config: inout MigrationConfig)throws {
     config.add(migration: Street.self, database: .mysql)
+    config.add(migration: Address.self, database: .mysql)
 }
---
+++ Sources/App/Models/Address.swift
@@ -0,0 +1,40 @@
+import FluentMySQL
+
+// https://stackoverflow.com/questions/1159756/how-should-international-geographical-addresses-be-stored-in-a-relational-databa/1160031#1160031
+final class Address {
+    var id: Int?
+    
+    var buildingName: String?
+    var typeIdentifier: String?
+    var type: String?
+    var municipality: String?
+    var city: String?
+    var district: String?
+    var postalArea: String?
+    var country: String?
+    
+    init(
+        buildingName: String?,
+        typeIdentifier: String?,
+        type: String?,
+        municipality: String?,
+        city: String?,
+        district: String?,
+        postalArea: String?,
+        country: String?
+    ) {
+        self.id = nil
+        
+        self.buildingName = buildingName
+        self.typeIdentifier = typeIdentifier
+        self.type = type
+        self.municipality = municipality
+        self.city = city
+        self.district = district
+        self.postalArea = postalArea
+        self.country = country
+    }
+}
+
+extension Address: Migration { }
+extension Address: MySQLModel { }
