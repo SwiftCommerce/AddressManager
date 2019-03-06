6a61485891405c0afa2ee0b4b089bfc1b131f935
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-06 09:39:25 -0600

    Installed fluent-mysql package

--- Package.swift
+++ Package.swift
@@ -5,10 +5,11 @@ import PackageDescription
 let package = Package(
     name: "AddressManager",
     dependencies: [
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.1"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "App", dependencies: ["Vapor", "FluentMySQL"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
