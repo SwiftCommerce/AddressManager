0d9e7d9f8414488eb8ccc502bd5373cd24a44f6f
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-07 10:02:03 -0600

    Installed skelpo/JSON package

--- Package.swift
+++ Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 let package = Package(
     name: "AddressManager",
     dependencies: [
+        .package(url: "https://github.com/skelpo/JSON.git", from: "0.13.1"),
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.1"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentMySQL"]),
+        .target(name: "App", dependencies: ["Vapor", "FluentMySQL", "JSON"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
