29cb3c79c4af820893590b10882c0fbb1d50fe2d
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-11 08:37:06 -0500

    Created POST /:/addresses/validate route

--- Sources/App/Controllers/AddressController.swift
+++ Sources/App/Controllers/AddressController.swift
@@ -14,26 +14,36 @@ final class AddressController: RouteCollection {
     func boot(router: Router) throws {
         let addresses = router.grouped(any, "addresses")
         
+        // CRUD
         addresses.post(AddressContent.self, use: create)
         addresses.get(Address.parameter, use: read)
         addresses.patch(JSON.self, at: Address.parameter, use: update)
         addresses.delete(Address.parameter, use: delete)
+        
+        // Helpers
+        addresses.post(AddressContent.self, at: "validate", use: validate)
     }
     
-    func create(_ request: Request, content: AddressContent)throws -> Future<AddressContent> {
+    
+    func create(_ request: Request, content: AddressContent)throws -> EventLoopFuture<AddressContent> {
         return self.repository.create(address: content)
     }
     
-    func read(_ request: Request)throws -> Future<AddressContent> {
+    func read(_ request: Request)throws -> EventLoopFuture<AddressContent> {
         return try self.repository.find(address: request.addressID()).unwrap(or: Abort(.notFound))
     }
     
-    func update(_ request: Request, content: JSON)throws -> Future<AddressContent> {
+    func update(_ request: Request, content: JSON)throws -> EventLoopFuture<AddressContent> {
         return try self.repository.update(address: request.addressID(), with: content).unwrap(or: Abort(.notFound))
     }
     
-    func delete(_ request: Request)throws -> Future<HTTPStatus> {
+    func delete(_ request: Request)throws -> EventLoopFuture<HTTPStatus> {
         return try self.repository.delete(address: request.addressID()).transform(to: .noContent)
+    }
+    
+    
+    func validate(_ request: Request, content: AddressContent)throws -> EventLoopFuture<HTTPStatus> {
+        return self.validator.validate(address: content).transform(to: .ok)
     }
 }
 
