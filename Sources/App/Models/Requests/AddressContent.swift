e83b9153bf26f4f9230677ffe12313433498182e
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-13 12:59:33 -0500

    Moved URL encoding logic to AddressContent.urlEncode(separator:)

--- Sources/App/Models/Requests/AddressContent.swift
+++ Sources/App/Models/Requests/AddressContent.swift
@@ -1,5 +1,4 @@
 import Vapor
-import Fluent
 
 struct AddressContent: Content {
     var id: Int?
@@ -12,4 +11,32 @@ struct AddressContent: Content {
     var postalArea: String?
     var country: String?
     var street: StreetContent?
+    
+    func urlEncoded(separator: String = "+")throws -> String {
+        let street = [
+            self.street?.number.map(String.init),
+            self.buildingName,
+            self.street?.numberSuffix,
+            self.street?.name,
+            self.street?.type,
+            self.street?.direction?.rawValue
+        ].compactMap { $0 }.joined(separator: separator)
+        
+        let query = [
+            street,
+            self.type,
+            self.typeIdentifier,
+            self.municipality,
+            self.city,
+            self.district,
+            self.postalArea,
+            self.country
+        ].compactMap { $0 }.joined(separator: separator)
+        
+        guard let encoded = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            throw Abort(.badRequest, reason: "Could not percent-encode URL query string: `\(query)`")
+        }
+        
+        return encoded
+    }
 }
